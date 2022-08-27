21b1064e008f81ebfd28cd1ac6b2d7a2ca0ea186
vinit0711 <deovineet@gmail.com>
2022-08-27 23:02:41 +0530

    first commit

---
+++ src/main/scala/googleAdSearches.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object googleAdSearches extends App {
+  val appName: String = "googleAdsSearches" //The name of Spark application on WebUI
+  val master: String = "local[*]" // Spark, Mesos or YARN cluster URL
+  val conf = new SparkConf().setAppName(appName).setMaster(master)
+  val sc = new SparkContext(conf)
+  //  code
+  val input = sc.textFile("/home/infinity/Documents/Spark/trendy/trendy_course_material/dataset/bigDataCampaign.csv")
+  val flattenrdd = input.map(x=>(x.split(",")(10).toFloat,x.split(",")(0)))
+
+    val trimrdd=flattenrdd.flatMapValues(x=>x.split(" "))
+  val rdd3=trimrdd.map(x=>(x._2,x._1))
+  //  val wordsLower=rdd2.map(x=>x.toLowerCase())
+  //  val rdd3 = wordsLower.map(x => (x, 1))
+    val rdd4 = rdd3.reduceByKey((x, y) => x + y)
+  rdd4.collect.foreach(println)
+}
